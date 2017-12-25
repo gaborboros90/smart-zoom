fb839e8dea0d03c5089894b47b904b2b6dd7a7e5
Gabor Boros <gabor_boros@epam.com>
2017-12-25 21:08:47 +0100

    SM-2: Template loading sccs compile added to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,42 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-    entry: './lib/smart.zoom.module.js',
-    output: {
-        filename: 'smart.zoom.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: ['babel-loader']
-            }
-        ]
-    },
+	entry: './lib/smart.zoom.module.js',
+	output: {
+		filename: 'smart.zoom.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: ['babel-loader']
+			},
+			{
+				test: /\.scss$/,
+				use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: [
+						'css-loader',
+						'sass-loader'
+					]
+				})
+			},
+			{
+				test: /\.html$/,
+				exclude: /node_modules/,
+				loader: 'raw-loader'
+			}
+		]
+	},
+	plugins: [
+		new ExtractTextPlugin('smart.zoom.css'),
+	],
 	devtool: 'inline-source-map',
-    devServer: {
-        port: 3000,
-        contentBase: './'
-    }
+	devServer: {
+		port: 3000,
+		contentBase: './'
+	}
 };
